b23813661a7ada9f986027b6bd2962a85a99d379
Kenji Shimizu <jrywm121@gmail.com>
2019-06-20 23:57:52 +0900

    add bubble sort

---
+++ bubble-sort/README.md
@@ -0,0 +1,11 @@
+# Bubble Sort
+
+## description
+
+[this article](https://www.geeksforgeeks.org/bubble-sort/) is very useful for understanding.
+
+Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping the adjacent elements if they are in wrong order.
+
+## Time Complexity
+
+O(n^2)
---
+++ bubble-sort/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	arr := []int{5, 3, 2, 4, 1}
+	count := sort(arr)
+	fmt.Println("sorted array:", arr)
+	fmt.Println("inversion count:", count)
+}
+
+func sort(arr []int) int {
+	var count int
+	for i := len(arr) - 1; i < len(arr)-2; i++ {
+		for j := len(arr) - 1; j > i; i-- {
+			if arr[j] < arr[j-1] {
+				arr[j], arr[j-1] = arr[j-1], arr[j]
+				count++
+			}
+		}
+	}
+	return count
+}
