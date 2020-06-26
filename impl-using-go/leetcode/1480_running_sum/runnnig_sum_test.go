d1fcab1cb47b42fb41e92da235a0aa7ee9906c34
Kenji Shimizu <jrywm121@gmail.com>
2020-06-27 08:17:16 +0900

    テストケースをサブテストに分割

--- impl-using-go/leetcode/1480_running_sum/runnnig_sum_test.go
+++ impl-using-go/leetcode/1480_running_sum/runnnig_sum_test.go
@@ -10,17 +10,22 @@ func TestRunningSum(t *testing.T) {
 	t.Parallel()
 
 	tests := []struct {
+		name  string
 		input []int
 		want  []int
 	}{
-		{[]int{1, 2, 3, 4}, []int{1, 3, 6, 10}},
-		{[]int{1, 1, 1, 1, 1}, []int{1, 2, 3, 4, 5}},
-		{[]int{3, 1, 2, 10, 1}, []int{3, 4, 6, 16, 17}}}
+		{"4 elements", []int{1, 2, 3, 4}, []int{1, 3, 6, 10}},
+		{"1 * 5 elements", []int{1, 1, 1, 1, 1}, []int{1, 2, 3, 4, 5}},
+		{"5 elements", []int{3, 1, 2, 10, 1}, []int{3, 4, 6, 16, 17}}}
 
 	for _, tt := range tests {
-		got := runningSum(tt.input)
-		if diff := cmp.Diff(tt.want, got); diff != "" {
-			t.Errorf(diff)
-		}
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			got := runningSum(tt.input)
+			if diff := cmp.Diff(tt.want, got); diff != "" {
+				t.Errorf(diff)
+			}
+		})
 	}
 }
