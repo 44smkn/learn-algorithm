659f4b803becfa1a2e40b8ab3c6eb4fff7a3f705
Kenji Shimizu <jrywm121@gmail.com>
2020-07-05 12:26:21 +0900

    2分探索のテストを追加

--- impl-using-go/aoj/search/search_test.go
+++ impl-using-go/aoj/search/search_test.go
@@ -4,6 +4,38 @@ import (
 	"testing"
 )
 
+func TestBinarySearch(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name string
+		s    []int
+		t    []int
+		want int
+	}{
+		{"case1", []int{1, 2, 3, 4, 5}, []int{3, 4, 1}, 3},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			got := multipleBinarySearch(tt.s, tt.t)
+			if tt.want != got {
+				t.Errorf("want: %v  got: %v", tt.want, got)
+			}
+		})
+	}
+}
+
+func multipleBinarySearch(s []int, t []int) int {
+	var count int
+	for _, v := range t {
+		if exists := binarySearch(s, v); exists {
+			count++
+		}
+	}
+	return count
+}
+
 func initVar() (a, q []int, n int) {
 	a = makeRange(0, 10000000)
 	q = []int{3890000, 4900005, 12890000, 9808009}
